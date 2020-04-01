ebc728fec3fee80e2ec4452830b8d7c9f69002af
Vlad Elagin <vlad.elagin@keenethics.com>
2020-04-01 18:30:42 +0300

    enhanced chart with axis, minor refactoring

--- src/renderer/d3/barChart.ts
+++ src/renderer/d3/barChart.ts
@@ -1,40 +1,112 @@
 import * as d3 from 'd3';
-import { BarChartData } from '../../types/charts';
+import { BarChartData, BarChartDimensions, ChartMargins } from '../../types/charts';
 
 class BarChartSvg {
-  private svg: d3.Selection<SVGSVGElement, unknown, null, undefined>;
+  private svg: d3.Selection<SVGGElement, unknown, null, undefined>;
 
   private data: BarChartData;
 
-  private dimensions: { width: number; height: number; barWidth: number; barMargin: number } = {
-    width: 500,
-    height: 300,
+  private x: d3.ScaleBand<string>;
+
+  private y: d3.ScaleLinear<number, number>;
+
+  private xAxis: any;
+
+  private yAxis: any;
+
+  private margins: ChartMargins = {
+    top: 10,
+    right: 10,
+    bottom: 20,
+    left: 20
+  };
+
+  private dimensions: BarChartDimensions = {
+    width: 500 - this.margins.left - this.margins.right,
+    height: 230 - this.margins.top - this.margins.bottom,
     barWidth: 50,
-    barMargin: 30
+    barPadding: 0.4
   };
 
   constructor(node: HTMLElement, data: BarChartData) {
+    // Create svg wrapper for chart
     this.svg = d3
       .select(node)
       .append('svg')
-      .attr('width', this.dimensions.width)
-      .attr('height', this.dimensions.height);
+      .attr('width', this.dimensions.width + this.margins.left + this.margins.right)
+      .attr('height', this.dimensions.height + this.margins.top + this.margins.bottom)
+      .append('g')
+      .attr('transform', `translate(${this.margins.left}, ${this.margins.top})`);
+
     this.data = data;
 
+    this.y = this.getYScale();
+    this.x = this.getXScale();
+    this.xAxis = d3.axisBottom(this.x);
+    this.yAxis = d3.axisLeft(this.y);
+
     this.drawBars();
+    this.drawAxis();
   }
 
+  /**
+   * Create X axis scale.
+   * Calculates bars position depending on their width and margins
+   */
+  private getXScale = () => {
+    return (
+      d3
+        .scaleBand()
+        .domain(this.data.map(d => d.name))
+        .range([0, this.dimensions.width])
+        // barPadding isn't strictly required to make dimensions interface
+        // compatible to another types of charts
+        .padding(this.dimensions.barPadding as number)
+    );
+  };
+
+  /**
+   * Create Y axis scale.
+   * Calculates height of bars depending on data
+   */
+  private getYScale = () => {
+    const minAge: number = d3.min(this.data, i => i.age) as number;
+    const maxAge: number = d3.max(this.data, i => i.age) as number;
+    return d3
+      .scaleLinear()
+      .domain([minAge * 0.5, maxAge])
+      .range([this.dimensions.height, minAge]);
+  };
+
+  /**
+   * Draw bars depending on data
+   */
   private drawBars = () => {
     this.svg
       .selectAll('rect')
       .data(this.data)
       .enter()
       .append('rect')
-      .attr('x', (d, i) => i * this.dimensions.barWidth + i * this.dimensions.barMargin)
-      .attr('y', d => this.dimensions.height - d.age)
-      .attr('width', this.dimensions.barWidth)
-      .attr('height', d => d.age)
+      // type definitions seem to think that band scale can return number OR undefined
+      // add 'as number' to prevent compilation errors
+      .attr('x', d => this.x(d.name) as number)
+      .attr('y', d => this.y(d.age))
+      .attr('width', this.x.bandwidth)
+      .attr('height', d => this.dimensions.height - this.y(d.age))
       .attr('fill', 'grey');
+  };
+
+  /**
+   * Add axis to chart
+   */
+  private drawAxis = () => {
+    this.svg
+      .append('g')
+      // move axis to bottom
+      .attr('transform', `translate(0, ${this.dimensions.height})`)
+      .call(this.xAxis);
+
+    this.svg.append('g').call(this.yAxis);
   };
 }
 
--- src/types/charts.ts
+++ src/types/charts.ts
@@ -4,3 +4,22 @@ export interface BarChartDataItem {
 }
 
 export type BarChartData = BarChartDataItem[];
+
+export interface ChartDimensions {
+  width: number;
+  height: number;
+  barWidth?: number;
+  barPadding?: number;
+}
+
+export interface BarChartDimensions extends ChartDimensions {
+  barWidth: number;
+  barPadding: number;
+}
+
+export interface ChartMargins {
+  top: number;
+  right: number;
+  bottom: number;
+  left: number;
+}
